405a62fc3acbc75979adfd6d093c4d3625bb3e99
Yoonha Hwang <yoonha.highsummer@gmail.com>
2021-10-23 17:15:37 +0900

    Add chain or in must

--- src/must.ts
+++ src/must.ts
@@ -15,6 +15,18 @@ export class Must<E, A> {
     }
   }
 
+  chainOr<F, B>(f: (a: A) => Either<F, B> | Must<E | F, B>, or: B): Must<E | F, B> {
+    const result = f(this.a);
+    if (result instanceof Either) {
+      return result.getOrElse(
+        b => new Must<E | F, B>(b, this.e),
+        e => new Must<E | F, B>(or, [...this.e, e])
+      )
+    } else {
+      return result.mapLeft(fs => [...this.e, ...fs])
+    }
+  }
+
   map<B>(f: (a: A) => B): Must<E, B> {
     return new Must(f(this.a), this.e)
   }
@@ -25,6 +37,18 @@ export class Must<E, A> {
       return result.getOrElse(
         b => new Must<E | F, A | B>(b, []),
         e => new Must<E | F, A | B>(this.a, [...this.e, e])
+      )
+    } else {
+      return result.mapLeft(fs => [...this.e, ...fs])
+    }
+  }
+
+  chainLeftOr<F, B>(f: (es: E[]) => Either<F, B> | Must<E | F, B>, or: B): Must<E | F, B> {
+    const result = f(this.e);
+    if (result instanceof Either) {
+      return result.getOrElse(
+        b => new Must<E | F, B>(b, []),
+        e => new Must<E | F, B>(or, [...this.e, e])
       )
     } else {
       return result.mapLeft(fs => [...this.e, ...fs])
